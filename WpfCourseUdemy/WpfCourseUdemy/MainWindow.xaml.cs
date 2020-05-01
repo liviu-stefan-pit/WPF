2dd9944ac338fc47e6f9f98003ecb0f5302f9fe2
liviustefan <liviustefan.petroaia@gmail.com>
2020-05-01 06:06:12 +0300

    Basic WPF Calculator

---
+++ WpfCourseUdemy/WpfCourseUdemy/MainWindow.xaml.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfCourseUdemy
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        #region Fields
+        double lastNumber;
+
+        double result;
+
+        SelectedOperator selectedOperator;
+        #endregion
+
+        #region Constructor
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            btnAC.Click += BtnAC_Click;
+            btnNegative1.Click += BtnNegative1_Click;
+            btnPercentage.Click += BtnPercentage_Click;
+            btnEqual.Click += BtnEqual_Click;
+        }
+        #endregion
+
+        #region Event Handlers
+        private void BtnEqual_Click(object sender, RoutedEventArgs e)
+        {
+            double newNumber;
+            if (double.TryParse(lblResult.Content.ToString(), out newNumber))
+            {
+                switch (selectedOperator)
+                {
+                    case SelectedOperator.Addition:
+                        result = SimpleMath.Add(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Substraction:
+                        result = SimpleMath.Substract(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Multiplication:
+                        result = SimpleMath.Multiply(lastNumber, newNumber);
+                        break;
+                    case SelectedOperator.Division:
+                        result = SimpleMath.Divide(lastNumber, newNumber);
+                        break;
+                }
+
+                lblResult.Content = result.ToString();
+            }
+        }
+
+        private void BtnPercentage_Click(object sender, RoutedEventArgs e)
+        {
+            double tempNumber;
+            if (double.TryParse(lblResult.Content.ToString(), out tempNumber))
+            {
+                tempNumber /= 100;
+                if (lastNumber != 0)
+                    tempNumber *= lastNumber;
+
+                lblResult.Content = tempNumber.ToString();
+            }
+        }
+
+        private void BtnNegative1_Click(object sender, RoutedEventArgs e)
+        {
+            if(double.TryParse(lblResult.Content.ToString(), out lastNumber))
+            {
+                lastNumber *= -1;
+                lblResult.Content = lastNumber.ToString();
+            }
+        }
+
+        private void BtnAC_Click(object sender, RoutedEventArgs e)
+        {
+            lblResult.Content = "0";
+            result = 0;
+            lastNumber = 0;
+        }
+
+        private void OperationBtn_Click(object sender, RoutedEventArgs e)
+        {
+            if (double.TryParse(lblResult.Content.ToString(), out lastNumber))
+            {
+                lblResult.Content = "0";
+            }
+
+            if (sender == btnPlus)
+                selectedOperator = SelectedOperator.Addition;
+            if (sender == btnMinus)
+                selectedOperator = SelectedOperator.Substraction;
+            if (sender == btnStar)
+                selectedOperator = SelectedOperator.Multiplication;
+            if (sender == btnDivision)
+                selectedOperator = SelectedOperator.Division;
+        }
+
+        private void NumberBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int selectedValue = 0;
+
+            if (sender == btn0)
+                selectedValue = 0;
+            if (sender == btn1)
+                selectedValue = 1;
+            if (sender == btn2)
+                selectedValue = 2;
+            if (sender == btn3)
+                selectedValue = 3;
+            if (sender == btn4)
+                selectedValue = 4;
+            if (sender == btn5)
+                selectedValue = 5;
+            if (sender == btn6)
+                selectedValue = 6;
+            if (sender == btn7)
+                selectedValue = 7;
+            if (sender == btn8)
+                selectedValue = 8;
+            if (sender == btn9)
+                selectedValue = 9;
+
+            if (lblResult.Content.ToString() == "0")
+            {
+                lblResult.Content = $"{selectedValue}";
+            }
+            else
+            {
+                lblResult.Content = $"{lblResult.Content}{selectedValue}";
+            }
+        }
+
+        private void btnDot_Click(object sender, RoutedEventArgs e)
+        {
+            if (!lblResult.Content.ToString().Contains("."))
+                lblResult.Content = $"{lblResult.Content}.";
+        }
+        #endregion
+    }
+
+    public enum SelectedOperator
+    {
+        Addition,
+        Substraction,
+        Multiplication,
+        Division
+    }
+
+    public class SimpleMath
+    {
+        public static double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public static double Substract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public static double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public static double Divide(double a, double b)
+        {
+            if (b == 0)
+            {
+                MessageBox.Show("Division by 0 is not supported", "Wrong operation", MessageBoxButton.OK, MessageBoxImage.Error);
+                return 0;
+            }
+
+            return a / b;
+        }
+    }
+}
