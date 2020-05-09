12dc4299388bfa9dc83e30c73b9bbf7a93560b4f
liviustefan <liviustefan.petroaia@gmail.com>
2020-05-09 11:33:01 +0300

    WeatherApp - Finish

--- WpfCourseUdemy/WheaterApp - MVVM/Model/Units.cs
+++ WpfCourseUdemy/WheaterApp - MVVM/Model/Units.cs
@@ -8,7 +8,7 @@ namespace WheaterApp___MVVM.Model
 {
     public class Units
     {
-        public double Value { get; set; }
+        public string Value { get; set; }
         public string Unit { get; set; }
         public int UnitType { get; set; }
     }
---
+++ WpfCourseUdemy/WheaterApp - MVVM/ViewModel/WeatherVM.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WheaterApp___MVVM.Model;
+using WheaterApp___MVVM.ViewModel.Commands;
+using WheaterApp___MVVM.ViewModel.Helpers;
+
+namespace WheaterApp___MVVM.ViewModel
+{
+    public class WeatherVM : INotifyPropertyChanged
+    {
+        private string query;
+
+        public string Query
+        {
+            get { return query; }
+            set { query = value; OnPropertyChanged("Query"); }
+        }
+
+        private CurrentConditions currentConditions;
+
+        public CurrentConditions CurrentConditions
+        {
+            get { return currentConditions; }
+            set { currentConditions = value; OnPropertyChanged("CurrentConditions"); }
+        }
+
+        private City selectedCity;
+
+        public City SelectedCity
+        {
+            get { return selectedCity; }
+            set
+            { 
+                selectedCity = value; 
+
+                if(selectedCity != null)
+                {
+                    OnPropertyChanged("SelectedCity");
+                    GetCurrentConditions();
+                }
+            }
+        }
+
+        public SearchCommand SearchCommand { get; set; }
+
+        public ObservableCollection<City> Cities { get; set; }
+
+        public WeatherVM()
+        {
+            SearchCommand = new SearchCommand(this);
+            Cities = new ObservableCollection<City>();
+        }
+
+        private async void GetCurrentConditions()
+        {
+            Query = string.Empty;
+            CurrentConditions = await AccuWeatherHelper.GetCurrentConditions(SelectedCity.Key);
+            Cities.Clear();
+        }
+
+        public async void MakeQuery()
+        {
+            var cities = await AccuWeatherHelper.GetCities(Query);
+
+            Cities.Clear();
+            foreach(var city in cities)
+            {
+                Cities.Add(city);
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void OnPropertyChanged(string propName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
